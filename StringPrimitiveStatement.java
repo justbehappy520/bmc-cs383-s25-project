50d9deaae5e107eb1c066238d97e5cdb0e76eaf8
rlassman <rebeccalassman@gmail.com>
2025-05-07 11:08:08 -0400

    new

--- StringPrimitiveStatement.java
+++ StringPrimitiveStatement.java
@@ -20,14 +20,6 @@
 
 package org.evosuite.testcase.statements;
 
-import org.evosuite.Properties;
-import org.evosuite.seeding.ConstantPool;
-import org.evosuite.seeding.ConstantPoolManager;
-import org.evosuite.testcase.TestCase;
-import org.evosuite.testcase.execution.CodeUnderTestException;
-import org.evosuite.testcase.execution.Scope;
-import org.evosuite.utils.Randomness;
-
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -39,162 +31,165 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.evosuite.Properties;
+import org.evosuite.seeding.ConstantPool;
+import org.evosuite.seeding.ConstantPoolManager;
+import org.evosuite.testcase.TestCase;
+import org.evosuite.testcase.execution.CodeUnderTestException;
+import org.evosuite.testcase.execution.Scope;
+import org.evosuite.utils.Randomness;
+
 /**
  * <p>
  * StringPrimitiveStatement class.
  * </p>
- *
+ * 
  * @author fraser
  */
 public class StringPrimitiveStatement extends PrimitiveStatement<String> {
 
-    private static final long serialVersionUID = 274445526699835887L;
+	private static final long serialVersionUID = 274445526699835887L;
 
-    /**
-     * <p>
-     * Constructor for StringPrimitiveStatement.
-     * </p>
-     *
-     * @param tc    a {@link org.evosuite.testcase.TestCase} object.
-     * @param value a {@link java.lang.String} object.
-     */
-    public StringPrimitiveStatement(TestCase tc, String value) {
-        super(tc, String.class, value);
-    }
+	/**
+	 * <p>
+	 * Constructor for StringPrimitiveStatement.
+	 * </p>
+	 * 
+	 * @param tc
+	 *            a {@link org.evosuite.testcase.TestCase} object.
+	 * @param value
+	 *            a {@link java.lang.String} object.
+	 */
+	public StringPrimitiveStatement(TestCase tc, String value) {
+		super(tc, String.class, value);
+	}
 
-    /**
-     * <p>
-     * Constructor for StringPrimitiveStatement.
-     * </p>
-     *
-     * @param tc a {@link org.evosuite.testcase.TestCase} object.
-     */
-    public StringPrimitiveStatement(TestCase tc) {
-        super(tc, String.class, "");
-    }
+	/**
+	 * <p>
+	 * Constructor for StringPrimitiveStatement.
+	 * </p>
+	 * 
+	 * @param tc
+	 *            a {@link org.evosuite.testcase.TestCase} object.
+	 */
+	public StringPrimitiveStatement(TestCase tc) {
+		super(tc, String.class, "");
+	}
 
-    /* (non-Javadoc)
-     * @see org.evosuite.testcase.PrimitiveStatement#zero()
-     */
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.PrimitiveStatement#zero()
+	 */
+	/** {@inheritDoc} */
+	@Override
+	public void zero() {
+		value = "";
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void zero() {
-        value = "";
-    }
+	private static String removeCharAt(String s, int pos) {
+		return s.substring(0, pos) + s.substring(pos + 1);
+	}
 
-    private static String removeCharAt(String s, int pos) {
-        return s.substring(0, pos) + s.substring(pos + 1);
-    }
+	private static String replaceCharAt(String s, int pos, char c) {
+		return s.substring(0, pos) + c + s.substring(pos + 1);
+	}
 
-    private static String replaceCharAt(String s, int pos, char c) {
-        return s.substring(0, pos) + c + s.substring(pos + 1);
-    }
+	private static String insertCharAt(String s, int pos, char c) {
+		return s.substring(0, pos) + c + s.substring(pos);
+	}
 
-    private static String insertCharAt(String s, int pos, char c) {
-        return s.substring(0, pos) + c + s.substring(pos);
-    }
+	private String StringInsert(String s, int pos) {
+		final double ALPHA = 0.5;
+		int count = 1;
 
-    private String StringInsert(String s, int pos) {
-        final double ALPHA = 0.5;
-        int count = 1;
+		while (Randomness.nextDouble() <= Math.pow(ALPHA, count)
+		        && s.length() < Properties.STRING_LENGTH) {
+			count++;
+			// logger.info("Before insert: '"+s+"'");
+			s = insertCharAt(s, pos, Randomness.nextChar());
+			// logger.info("After insert: '"+s+"'");
+		}
+		return s;
+	}
 
-        while (Randomness.nextDouble() <= Math.pow(ALPHA, count)
-                && s.length() < Properties.STRING_LENGTH) {
-            count++;
-            // logger.info("Before insert: '"+s+"'");
-            s = insertCharAt(s, pos, Randomness.nextChar());
-            // logger.info("After insert: '"+s+"'");
-        }
-        return s;
-    }
+	/** {@inheritDoc} */
+	@Override
+	public void delta() {
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void delta() {
+		String s = value;
+		if(s == null) {
+			randomize();
+			return;
+		}
+		
+		final double P2 = 1d / 3d;
+		double P = 1d / s.length();
+		// Delete
+		if (Randomness.nextDouble() < P2) {
+			for (int i = s.length(); i > 0; i--) {
+				if (Randomness.nextDouble() < P) {
+					// logger.info("Before remove at "+i+": '"+s+"'");
+					s = removeCharAt(s, i - 1);
+					// logger.info("After remove: '"+s+"'");
+				}
+			}
+		}
+		P = 1d / s.length();
+		// Change
+		if (Randomness.nextDouble() < P2) {
+			for (int i = 0; i < s.length(); i++) {
+				if (Randomness.nextDouble() < P) {
+					// logger.info("Before change: '"+s+"'");
+					s = replaceCharAt(s, i, Randomness.nextChar());
+					// logger.info("After change: '"+s+"'");
+				}
+			}
+		}
 
-        String s = value;
-        if (s == null) {
-            randomize();
-            return;
-        }
+		// Insert
+		if (Randomness.nextDouble() < P2) {
+			// for(int i = 0; i < s.length(); i++) {
+			// if(Randomness.nextDouble() < P) {
+			int pos = 0;
+			if (s.length() > 0)
+				pos = Randomness.nextInt(s.length());
+			s = StringInsert(s, pos);
+			// }
+			// }
+		}
+		value = s;
+		// logger.info("Mutated string now is: "+value);
+	}
 
-        final double P2 = 1d / 3d;
-        double P = 1d / s.length();
-        // Delete
-        if (Randomness.nextDouble() < P2) {
-            for (int i = s.length(); i > 0; i--) {
-                if (Randomness.nextDouble() < P) {
-                    // logger.info("Before remove at "+i+": '"+s+"'");
-                    s = removeCharAt(s, i - 1);
-                    // logger.info("After remove: '"+s+"'");
-                }
-            }
-        }
-        P = 1d / s.length();
-        // Change
-        if (Randomness.nextDouble() < P2) {
-            for (int i = 0; i < s.length(); i++) {
-                if (Randomness.nextDouble() < P) {
-                    // logger.info("Before change: '"+s+"'");
-                    s = replaceCharAt(s, i, Randomness.nextChar());
-                    // logger.info("After change: '"+s+"'");
-                }
-            }
-        }
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.PrimitiveStatement#increment(java.lang.Object)
+	 */
+	/**
+	 * <p>
+	 * increment
+	 * </p>
+	 */
+	public void increment() {
+		String s = value;
+		if(s == null) {
+			randomize();
+			return;
+		}
+		else if (s.isEmpty()) {
+			s += Randomness.nextChar();
+		} else {
+			s = replaceCharAt(s, Randomness.nextInt(s.length()), Randomness.nextChar());
+		}
 
-        // Insert
-        if (Randomness.nextDouble() < P2) {
-            // for(int i = 0; i < s.length(); i++) {
-            // if(Randomness.nextDouble() < P) {
-            int pos = 0;
-            if (s.length() > 0)
-                pos = Randomness.nextInt(s.length());
-            s = StringInsert(s, pos);
-            // }
-            // }
-        }
-        value = s;
-        // logger.info("Mutated string now is: "+value);
-    }
+		value = s;
+	}
 
-    /* (non-Javadoc)
-     * @see org.evosuite.testcase.PrimitiveStatement#increment(java.lang.Object)
-     */
-
-    /**
-     * <p>
-     * increment
-     * </p>
-     */
-    public void increment() {
-        String s = value;
-        if (s == null) {
-            randomize();
-            return;
-        } else if (s.isEmpty()) {
-            s += Randomness.nextChar();
-        } else {
-            s = replaceCharAt(s, Randomness.nextInt(s.length()), Randomness.nextChar());
-        }
-
-        value = s;
-    }
-
-    /* (non-Javadoc)
-     * @see org.evosuite.testcase.PrimitiveStatement#randomize()
-     */
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void randomize() {
-        List<String> lines = new ArrayList<>();
+	/* (non-Javadoc)
+	 * @see org.evosuite.testcase.PrimitiveStatement#randomize()
+	 */
+	/** {@inheritDoc} */
+	@Override
+	public void randomize() {
+		List<String> lines = new ArrayList<>();
         Path path = Paths.get("in.txt"); // read in the input file
 
         try { // handle exceptions
@@ -236,6 +231,7 @@ public class StringPrimitiveStatement extends PrimitiveStatement<String> {
             int negative = 0;
             int underscore = 0;
             int decimal = 0;
+			int randString = 0;
             for (int i = 0; i < lines.size(); i++) {
                 line = lines.get(i);
                 String[] parts = str.split(": ");
@@ -250,68 +246,122 @@ public class StringPrimitiveStatement extends PrimitiveStatement<String> {
                     unicode++;
                 } else if (pool.equals(DecimalPool)) {
                     decimal++;
-                }
+                } else if (pool.equals(UnderscorePool)) {
+					underscore++;
+				} else if (pool.equals(StringPool)) {
+					randString++;
+				}
             }
 
-            // pick one line at random
-            int index = Randomness.nextInt(lines.size());
-            value = lines.get(index);
+			double probDate = date/7;
+			double probHex = hex/7;
+			double probUni = unicode/7;
+			double probNeg = negative/7;
+			double probUnder = underscore/7;
+			double probDec = decimal/7;
+			double probRandString = randString/7;
 
-            double p = Randomness.nextDouble();
+			double dateRange = probDate;
+			double hexRange = dateRange + probHex;
+			double uniRange = hexRange + probUni;
+			double negRange = uniRange + probNeg;
+			double underRange = negRange + probUnder;
+			double decRange = underRange + probDec;
 
-            if (p <= 0.5) {
-                
-            } else { // calls a constant pool or generates a random input
-                if (p >= Properties.PRIMITIVE_POOL) {// generates a random input
-                    value = Randomness.nextString(Randomness.nextInt(Properties.STRING_LENGTH));
-                } else { // calls a constant pool
-                    ConstantPool constantPool = ConstantPoolManager.getInstance().getConstantPool();
-                    String candidateString = constantPool.getRandomString();
-                    if(Properties.MAX_STRING > 0 && candidateString.length() < Properties.MAX_STRING) {
-                        value = candidateString;
-                    } else {
-                        value = Randomness.nextString(Randomness.nextInt(Properties.STRING_LENGTH));
-                    }
-                }
-            }
-        }
-    }
+			CustomPools customPool = new CustomPools();
+			double p = Randomness.nextDouble();
+			if (p <= dateRange) {
+				value = customPool.randomDate();
+			} else if (p <= hexRange) {
+				value = customPool.randomHex();
+			} else if (p <= uniRange) {
+				value = customPool.randomUnicode();
+			} else if (p <= negRange) {
+				value = customPool.randomNegativeNumber();
+			} else if (p <= underRange) {
+				value = customPool.randomUnderscoreString();
+			} else if (p <= decRange) {
+				value = customPool.randomLongDecimal();
+			} else {
+				if (p >= Properties.PRIMITIVE_POOL) // generates a random string
+					value = Randomness.nextString(Randomness.nextInt(Properties.STRING_LENGTH));
+				else { // pulls from a constant pool
+					ConstantPool constantPool = ConstantPoolManager.getInstance().getConstantPool();
+					String candidateString = constantPool.getRandomString();
+					if(Properties.MAX_STRING > 0 && candidateString.length() < Properties.MAX_STRING)
+						value = candidateString;
+					else // generates a random string
+						value = Randomness.nextString(Randomness.nextInt(Properties.STRING_LENGTH));
+				}
+			}
 
-    @Override
-    public Throwable execute(Scope scope, PrintStream out)
-            throws InvocationTargetException, IllegalArgumentException,
-            IllegalAccessException, InstantiationException {
-        Throwable exceptionThrown = null;
+		}
 
-        try {
-            if (value == null)
-                retval.setObject(scope, null);
-            else {
-                // String literals may not be longer than 32767
-                if (value.length() >= 32767)
-                    throw new CodeUnderTestException(new IllegalArgumentException("Maximum string length exceeded"));
+            
+		/*double p = Randomness.nextDouble();
 
-                // In the JUnit code we produce, strings are generated as
-                // String foo = "bar";
-                // That means any reference comparison will behave different
-                // as internally value is created as String foo = new String("bar").
-                // Therefore we have to use the string object in the constant pool
-                retval.setObject(scope, value.intern());
-            }
-        } catch (CodeUnderTestException e) {
-            exceptionThrown = e;
-        }
-        return exceptionThrown;
-    }
+		if (p <= 0.5) {
+			System.out.println("hex\n");
+			value = "80000000";
+		} else if (p <= 0.6) {
+			System.out.println("date\n");
+			value = "2005-05-13aa"; //maybe change later
+		} else if (p <= 0.7) {
+			System.out.println("unicode\n");
+			value = "\uD83D\uDE30";
+		} else if (p <= 0.8) {
+			System.out.println("neg\n");
+			value = "-1";
+		} else {
+			System.out.println("other\n");
+			if (p >= Properties.PRIMITIVE_POOL)
+				value = Randomness.nextString(Randomness.nextInt(Properties.STRING_LENGTH));
+			else {
+				ConstantPool constantPool = ConstantPoolManager.getInstance().getConstantPool();
+				String candidateString = constantPool.getRandomString();
+				if(Properties.MAX_STRING > 0 && candidateString.length() < Properties.MAX_STRING)
+					value = candidateString;
+				else
+					value = Randomness.nextString(Randomness.nextInt(Properties.STRING_LENGTH));
+			}
+		}*/
+	}
 
-    private void writeObject(ObjectOutputStream oos) throws IOException {
-        oos.defaultWriteObject();
-        oos.writeObject(value);
-    }
+	@Override
+	public Throwable execute(Scope scope, PrintStream out)
+	        throws InvocationTargetException, IllegalArgumentException,
+	        IllegalAccessException, InstantiationException {
+		Throwable exceptionThrown = null;
 
-    private void readObject(ObjectInputStream ois) throws ClassNotFoundException,
-            IOException {
-        ois.defaultReadObject();
-        value = (String) ois.readObject();
-    }
+		try {
+			if(value == null)
+				retval.setObject(scope, null);
+			else {
+				// String literals may not be longer than 32767
+				if(value.length() >= 32767)
+					throw new CodeUnderTestException(new IllegalArgumentException("Maximum string length exceeded"));
+
+				// In the JUnit code we produce, strings are generated as
+				// String foo = "bar";
+				// That means any reference comparison will behave different
+				// as internally value is created as String foo = new String("bar").
+				// Therefore we have to use the string object in the constant pool
+				retval.setObject(scope, value.intern());
+			}
+		} catch (CodeUnderTestException e) {
+			exceptionThrown = e;
+		}
+		return exceptionThrown;
+	}
+	
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+		oos.defaultWriteObject();		
+		oos.writeObject(value);
+	}
+
+	private void readObject(ObjectInputStream ois) throws ClassNotFoundException,
+	        IOException {
+		ois.defaultReadObject();
+		value = (String) ois.readObject();
+	}
 }
